41b501129533646eb81d700f9ce918392373fc4a
¨Edgar <s.steinberg2019@gmail.com>
2023-08-02 19:00:22 -0300

    segundocommit

--- js/productos.js
+++ js/productos.js
@@ -1,8 +1,10 @@
+//Array de objetos
+
 let productos = [
     {
         id: 1,
         nombreProducto: "Big Solari",
-        descripcion: "Big Indio Solari Hamburguesa simple mas papas fritas",
+        descripcion: "Big Indio Solari: Hamburguesa simple mas papas fritas",
         precio: 3900,
         imagen: "./img/1.jpg",
         cantidad: 0
@@ -10,7 +12,7 @@ let productos = [
     {
         id: 2,
         nombreProducto: "Big Bellinson",
-        descripcion: "Big Bellinson Hamburguesa doblee mas papas fritas",
+        descripcion: "Big Bellinson: Hamburguesa doble mas papas fritas",
         precio: 3600,
         imagen: "./img/2.jpeg",
         cantidad: 0
@@ -18,7 +20,7 @@ let productos = [
     {
         id: 3,
         nombreProducto: "Big Poli",
-        descripcion: "Big Poli Hamburguesa simple caramelizada mas papas fritas",
+        descripcion: "Big Poli: Hamburguesa simple caramelizada mas papas fritas",
         precio: 3700,
         imagen: "./img/3.jpeg",
         cantidad: 0
@@ -26,7 +28,7 @@ let productos = [
     {
         id: 4,
         nombreProducto: "Big Chizzo",
-        descripcion: "Big Chizzo Hamburguesa doble extra cheddar mas papas fritas",
+        descripcion: "Big Chizzo: Hamburguesa doble cheddar mas papas fritas",
         precio: 3800,
         imagen: "./img/4.jpeg",
         cantidad: 0
@@ -34,14 +36,14 @@ let productos = [
     {
         id: 5,
         nombreProducto: "Big Tete",
-        descripcion: "Big Tete Hamburguesa simple mas papas fritas",
+        descripcion: "Big Tete: Hamburguesa simple mas papas fritas",
         precio: 3700,
         imagen: "./img/5.jpeg"
     },
     {
         id: 6,
         nombreProducto: "Big Pity",
-        descripcion: "Big Pity Hamburguesa doble mas papas fritas",
+        descripcion: "Big Pity: Hamburguesa doble mas papas fritas",
         precio: 3050,
         imagen: "./img/6.jpeg",
         cantidad: 0
@@ -49,7 +51,7 @@ let productos = [
     {
         id: 7,
         nombreProducto: "Big Pappo",
-        descripcion: "Big Pappo Hamburguesa doble mas papas fritas",
+        descripcion: "Big Pappo: Hamburguesa doble mas papas fritas",
         precio: 3350,
         imagen: "./img/7.jpeg",
         cantidad: 0
@@ -57,7 +59,7 @@ let productos = [
     {
         id: 8,
         nombreProducto: "Big Cerati",
-        descripcion: "Big Cerati Hamburguesa doble mas papas fritas",
+        descripcion: "Big Cerati: Hamburguesa doble mas papas fritas",
         precio: 3150,
         imagen: "./img/8.jpeg",
         cantidad: 0
--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-
+//capturo el ID
 let productCards = document.getElementById("product-cards")
+//creamos un array vacio
 let carrito = []
 
 
-
+//For con variable producto asignamos todo nuestro array
+//cardHTML con temple literal agregamos nuestra tarjeta y remplazamos los valores por los d nuestro array
+//a productCardsinnerHTML acedemos a nuestro div con el += le colocamos nuestras tarjetas y se agrega cada producto
 for (let i = 0; i < productos.length; i++) {
     let producto = productos[i]
 
     let cardHTML = `
     <div class="card" style="width: 18rem;">
-        <img src="${producto.imagen}" class="card-img-top" alt="...">
+        <img src="${producto.imagen}" class="card-img-top" alt="hamburguesas">
          <div class="card-body">
-            <h5 class="card-title">${producto.descripcion}</h5>
-          <p class="card-text">${producto.precio.toFixed(2)}</p>
+          <h5 class="card-title">${producto.descripcion}</h5>
+          <p class="card-text">Precio: $${producto.precio.toFixed(2)}</p>
           <input type="number" class="form-control mb-2" placeholder="Cantidad" value="0" data-producto-id="${producto.id}"> 
           <button class="btn btn-agregar-carrito" data-producto-id="${producto.id}">AGREGAR AL CARRITO</button>
         </div>
@@ -22,20 +25,29 @@ for (let i = 0; i < productos.length; i++) {
     productCards.innerHTML += cardHTML
 }
 
+//caputamos el input de la tarjeta
 let cantidadInputs = document.querySelectorAll('input[type="number"]')
 
+//lo recorremos con un for lo amacenamos en input creamos un evento y le pasamos la funcion actualizarcantidad
 for (let i = 0; i < cantidadInputs.length; i++) {
     let input = cantidadInputs[i]
     input.addEventListener("input", actualizarCantidad)
 }
 
+//Capturamos nuestros botones de la tajeta con su clase
 let agregarButtons = document.getElementsByClassName("btn-agregar-carrito")
 
+
+//Lo recorremos con el for lo almacenamos e button creamos un evento click y le pasamos la funcion agregaralcarrito
 for (let i = 0; i < agregarButtons.length; i++) {
     let button = agregarButtons[i]
     button.addEventListener("click", agregarAlCarrito)
 }
 
+//Esta funcion recibe parametro event que se lo agregamos al input capturamos la posicion de donde clickeamos con getatribue le asignamos un id
+//se parsea en la variable cantidad para que solo reciba numeros
+//productos find comparamos mi array de objetos con mi nuevo atributo 
+//producto.cantidad ya no es cero si no lo que coloca el usuario
 function actualizarCantidad(event) {
     let input = event.target
     let productoId = input.getAttribute("data-producto-id")
@@ -45,6 +57,34 @@ function actualizarCantidad(event) {
     producto.cantidad = cantidad
 }
 
+
+
+function actualizarCarrito() {
+    let carritoContenedor = document.getElementById("carrito")
+    carritoContenedor.innerHTML = ""
+
+    for (let producto of carrito) {
+        let { nombreProducto,descripcion,precio,cantidad } = producto
+        let cardCarrito = document.createElement("div")
+        cardCarrito.innerHTML = `
+            <h3>Producto: ${nombreProducto}</h3>
+            <h3>${descripcion}</h3>
+            <h3>Precio: $${precio.toFixed(2)}</h3>
+            <h3>Cantidad: ${cantidad}</h3>
+            <h3>Total de la compra: ${nombreProducto}: $${(producto.cantidad * producto.precio).toFixed(2)}</h3>
+        `
+        cardCarrito.className = "cardCarrito" 
+
+        carritoContenedor.appendChild(cardCarrito)
+    }
+}
+
+
+//Le colocamos un evento que lo almecena el button que .target captura la posicion y con getaatribute le asignamos un id
+//producto,id lo parseamos 
+//En productoencarrito aplicamos find que si item.id coinsicide con producto.id verificamos si el producto esta en el carrito
+//usamos condicionales q si el producto esta en el carrito me lo agregue y no me cree otro array nuevo pero si no, si que me agregue el producto al carrito
+// Tambien genere localstorage para que se almacen mi id alli y no se pieda la info, tmb un prompt para que cuando agregues al carrito te aparezca
 function agregarAlCarrito(event) {
     let button = event.target
     let productoId = button.getAttribute("data-producto-id")
@@ -53,15 +93,19 @@ function agregarAlCarrito(event) {
 
     let productoEnCarrito = carrito.find((item) => item.id == producto.id)
 
-    if (productoEnCarrito) {
-        productoEnCarrito += producto.cantidad
-    } else {
-        carrito.push({ ...producto })
-    }
 
-    console.log("producto agregado al carrito. ID: " + productoId)
+    productoEnCarrito ? productoEnCarrito.cantidad += producto.cantidad : carrito.push({...producto})
+
+
+    console.log(`producto agregado al carrito. ID:  ${productoId}`)
     console.log("carrito", carrito)
+
     localStorage.setItem("carrito", JSON.stringify(carrito));
+    
+    Swal.fire(
+        'Has agregado este producto al carrito!',  
+      )
+    actualizarCarrito()
 }
 
 
